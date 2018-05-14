bd62edbf078283b2a7e0699f541209d7fec09cc8
Michael Cruz <m.cruz@quarterspot.com>
2018-05-14 11:13:26 -0500

    updated DSCScript.ps1

---
+++ Azure/DSC/DSCscript.ps1
@@ -0,0 +1,26 @@
+Configuration ScriptTest
+{
+    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    Node 'localhost' {
+        Script UpdateConfigurationVersion
+        {
+            GetScript = {
+                $currentVersion = Get-Content (Join-Path -Path $env:SYSTEMDRIVE -ChildPath 'version.txt')
+                return @{ 'Result' = $currentVersion }
+            }
+            TestScript = {
+                $state = $GetScript
+                if( $state['Result'] -eq $using:version )
+                {
+                    Write-Verbose -Message ('{0} -eq {1}' -f $state['Result'],$using:version)
+                    return $true
+                }
+                Write-Verbose -Message ('Version up-to-date: {0}' -f $using:version)
+                return $false
+            }
+            SetScript = {
+                $using:version | Set-Content -Path (Join-Path -Path $env:SYSTEMDRIVE -ChildPath 'version.txt')
+            }
+        }
+    }
+}
