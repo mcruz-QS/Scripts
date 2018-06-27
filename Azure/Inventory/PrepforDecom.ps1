ecdf96c4ab051a7bc252da239bb063bdf5b32e33
Michael Cruz <m.cruz@quarterspot.com>
2018-06-27 10:45:32 -0500

    Azure Inventory

---
+++ Azure/Inventory/PrepforDecom.ps1
@@ -0,0 +1,76 @@
+﻿<#
+    Resource Group Name needed
+    Subscription Name needed
+#>
+QSconnectAZRM -Subscription 'LASO Development'
+$RG = Get-AzureRmResourceGroup -Name dcuergtrn000 
+$VMs = Get-AzureRmVM -ResourceGroupName $rg.ResourceGroupName
+((Get-AzureRmPublicIpAddress -ResourceGroupName $rg.ResourceGroupName).DnsSettings).DomainNameLabel
+foreach ($vm in $VMs){ Stop-AzureRmVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -Force -Confirm:$false}
+
+$resource = Get-AzureRmResource -ResourceGroupName $rg.ResourceGroupName
+$databases = $resource | where resourcetype -match "databases" 
+
+
+$DBServers = $resource | where resourcetype -EQ "Microsoft.Sql/servers"
+$DBServers | foreach {
+    $DBs = Get-AzureRmSqlDatabase -ServerName $_.Name -ResourceGroupName $RG.ResourceGroupName | where databasename -ne "master"
+    foreach ($DB in $DBS){
+        Set-AzureRmSqlDatabase -DatabaseName $DB.DatabaseName -ServerName $DB.ServerName -ResourceGroupName $DB.ResourceGroupName `
+            -Tags @{"Decom"="True"} -Edition Basic -Verbose 
+    }
+    # $DBs | select tags, Databasename, servername, resourcegroupname, Edition
+
+}
+
+$resource | foreach {
+    Set-AzureRmResource -ResourceGroupName $rg.ResourceGroupName -Tag @{
+        "Decom"="True";
+        "Envrionment"="Training"
+    } -ResourceName $_.Name -ResourceType $_.ResourceType -Force -Confirm:$False -AsJob
+}
+
+#region management
+
+$RG = Get-AzureRmResourceGroup -Name dcuergtrnmanagement 
+Set-AzureRmResourceGroup -Name $RG.ResourceGroupName -Tag @{
+        "Decom"="True";
+        "Envrionment"="Training"
+    }
+
+$resource = Get-AzureRmResource -ResourceGroupName $rg.ResourceGroupName
+$resource | foreach {
+    Set-AzureRmResource -ResourceGroupName $rg.ResourceGroupName -Tag @{
+        "Decom"="True";
+        "Envrionment"="Training"
+    } -ResourceName $_.Name -ResourceType $_.ResourceType -Force -Confirm:$False
+}
+#endregion
+
+#region Octoposh Disable
+$octoEnv = "Training"
+$VMs
+Set-OctopusConnectionInfo -Server $octo.UserName -ApiKey $octo.GetNetworkCredential().password
+
+$offlineVMOcto = foreach ($vm in $VMs){
+    (Get-OctopusEnvironment $octoEnv).machines | where {($_.status -eq "offline") -and ($_.name -match $vm.Name )}
+}
+# Set VM to Disabled in OctopusDeploy 
+
+$offlineVMOcto | foreach {
+    try{
+            $disableVM = get-octopusmachine $_.name
+            $disableVM.resource.isdisabled = $true
+            Update-OctopusResource -resource $disableVM.Resource
+
+            
+        }catch{
+            $Error[0]
+        }
+}
+#endregion
+
+#region Octoposh removal
+((Get-OctopusEnvironment $octoEnv).machines) | Remove-OctopusResource
+
+#endregion
---
+++ Azure/Inventory/get-resources.ps1
@@ -0,0 +1,57 @@
+﻿<#
+    List all resource groups in all subscriptions
+    List all resources in each resource group
+
+#>
+
+Connect-QSAzure -Subscription 'LASO Core Production' | out-null
+$subscriptions = $(Get-Content C:\Users\m.cruz\AppData\Local\qsazure\subscription.json | ConvertFrom-Json).nam
+$results = foreach ($subscription in $subscriptions) {
+    Connect-QSAzure -Subscription $subscription | out-null
+    $rgs= Get-AzureRmResourceGroup
+    write-verbose $rgs.count -Verbose
+    $rg = $null
+    foreach ($rg in $rgs){
+        Write-Verbose $rg.resourcegroupname -Verbose 
+        Get-AzureRmResource -ResourceGroupName $rg.ResourceGroupName
+    }
+}
+
+$res2 = foreach ($result in $results){
+    [PSCustomObject]@{
+        Name = $result.Name
+        ResourceGroupName = $result.resourceGroupName
+        Type = $result.Type
+        Location = $result.Location
+        ID = $result.ID
+        ParentResource = $result.ParentResource
+        Kind = $($result.Kind -replace ","," ")
+        skuName = $result.sku.Name
+        skuTier = $result.Sku.Tier
+        skuCapacity = $result.Sku.Capacity
+        tag = if (($result.Tags.keys) -gt ""){$($result.Tags).keys + " - " + $($result.Tags).values}else{$null}
+        planName = $result.Plan.name
+        planProduct = $result.Plan.Product
+        
+    }
+    
+}
+
+# Classic
+$TenantID = $(Get-Content C:\Users\m.cruz\AppData\Local\qsazure\subscription.json | ConvertFrom-Json)[0].TenantId
+Add-AzureAccount -Credential $azure -Tenant $TenantID
+$subscriptions = Get-AzureSubscription
+$results2 = foreach ($subscription in $subscriptions) {
+    Connect-QSAzure -Subscription $subscription | out-null
+    $rgs= Get-AzureRmResourceGroup
+    write-verbose $rgs.count -Verbose
+    $rg = $null
+    foreach ($rg in $rgs){
+        Write-Verbose $rg.resourcegroupname -Verbose 
+        Get-AzureRmResource -ResourceGroupName $rg.ResourceGroupName -ResourceType microsoft.ClassicCompute
+    }
+}
+
+
+
+Get-AzureSubscription
